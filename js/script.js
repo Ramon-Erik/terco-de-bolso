53982b96af42b5bc5362c7e74c9560f49caf8644
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-12-28 09:51:58 -0300

    expandir acordeon

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
+// const titulos = document.querySelectorAll('.acordeon .titulo');
+
+// titulos.forEach((titulo) => {
+//     titulo.classList.remove('active')
+//     titulo.addEventListener('click', function () {
+//         const conteudo = this.nextElementSibling;
+//         if (conteudo.style.maxHeight) {
+//             conteudo.style.maxHeight = null;
+//         } else {
+//             titulo.classList.add('active')
+//             conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
+//         }
+
+//         const todosConteudos = document.querySelectorAll('.acordeon .conteudo');
+//         todosConteudos.forEach((item) => {
+//             if (item !== conteudo && item.style.maxHeight) {
+//                 item.style.maxHeight = null;
+//             }
+//         });
+//     });
+// });
+
 const titulos = document.querySelectorAll('.acordeon .titulo');
 
 titulos.forEach((titulo) => {
-    titulo.addEventListener('click', function () {
-        const conteudo = this.nextElementSibling;
-        if (conteudo.style.maxHeight) {
-            conteudo.style.maxHeight = null;
-        } else {
-            conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
-        }
+  titulo.addEventListener('click', function() {
+    const conteudo = this.nextElementSibling;
+    this.classList.toggle('active');
 
-        const todosConteudos = document.querySelectorAll('.acordeon .conteudo');
-        todosConteudos.forEach((item) => {
-            if (item !== conteudo && item.style.maxHeight) {
-                item.style.maxHeight = null;
-            }
-        });
+    if (conteudo.style.maxHeight) {
+      conteudo.style.maxHeight = null;
+    } else {
+      conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
+    }
+
+    const todosTitulos = document.querySelectorAll('.acordeon .titulo');
+    todosTitulos.forEach((item) => {
+      if (item !== this && item.classList.contains('active')) {
+        item.classList.remove('active');
+        item.nextElementSibling.style.maxHeight = null;
+      }
     });
+  });
 });
