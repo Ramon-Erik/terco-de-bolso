be0885553bfe25fbd5319f11becebd0d4f57c5f1
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-12-28 16:16:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,22 @@
-// const titulos = document.querySelectorAll('.acordeon .titulo');
-
-// titulos.forEach((titulo) => {
-//     titulo.classList.remove('active')
-//     titulo.addEventListener('click', function () {
-//         const conteudo = this.nextElementSibling;
-//         if (conteudo.style.maxHeight) {
-//             conteudo.style.maxHeight = null;
-//         } else {
-//             titulo.classList.add('active')
-//             conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
-//         }
-
-//         const todosConteudos = document.querySelectorAll('.acordeon .conteudo');
-//         todosConteudos.forEach((item) => {
-//             if (item !== conteudo && item.style.maxHeight) {
-//                 item.style.maxHeight = null;
-//             }
-//         });
-//     });
-// });
-
-const titulos = document.querySelectorAll('.acordeon .titulo');
+tulos = document.querySelectorAll('.acordeon .titulo');
 
 titulos.forEach((titulo) => {
-  titulo.addEventListener('click', function() {
-    const conteudo = this.nextElementSibling;
-    this.classList.toggle('active');
+    titulo.addEventListener('click', function () {
+        const conteudo = this.nextElementSibling;
+        this.classList.toggle('active');
 
-    if (conteudo.style.maxHeight) {
-      conteudo.style.maxHeight = null;
-    } else {
-      conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
-    }
+        if (conteudo.style.maxHeight) {
+            conteudo.style.maxHeight = null;
+        } else {
+            conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
+        }
 
-    const todosTitulos = document.querySelectorAll('.acordeon .titulo');
-    todosTitulos.forEach((item) => {
-      if (item !== this && item.classList.contains('active')) {
-        item.classList.remove('active');
-        item.nextElementSibling.style.maxHeight = null;
-      }
+        const todosTitulos = document.querySelectorAll('.acordeon .titulo');
+        todosTitulos.forEach((item) => {
+            if (item !== this && item.classList.contains('active')) {
+                item.classList.remove('active');
+                item.nextElementSibling.style.maxHeight = null;
+            }
+        });
     });
-  });
 });
